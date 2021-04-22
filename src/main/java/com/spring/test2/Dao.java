6dc6800e98b4dea8bc5a50dad7c1a39d8b3a774e
JeonEuiMin <eumin@znit.kr>
2021-04-22 15:55:58 +0900

    프로젝트생성 및 데이터베이스 연동

---
+++ src/main/java/com/spring/test2/Dao.java
@@ -0,0 +1,28 @@
+package com.spring.test2;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Dao {
+	@Autowired
+	private DataSource dataSource;
+	
+	public Connection getCon() {
+		Connection conn = null;
+		
+		try {
+			conn = dataSource.getConnection();
+			
+		}catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return conn;
+		
+	}
+}
